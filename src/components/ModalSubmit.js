a08bf58b17f3e4c064c1173a31edd957bf4f947b
Tav Hafner <tav.hafner@gmail.com>
2018-02-11 13:32:16 -0700

    even more styling, almost done.

--- src/components/ModalSubmit.js
+++ src/components/ModalSubmit.js
@@ -2,12 +2,16 @@ import React from 'react';
 
 var ModalSubmit = ( { handleTextChange, handleNameChange, handleClick, name, id } ) => {
     return (
-        <div>
-            <h2> { name } </h2>
-            <input placeholder="text" onChange={(e) => handleTextChange(e.target.value) }/> 
+        <div className="modal-submit">
+            <h2 className="movie-title"> { name } </h2>
+            <textarea placeholder="Add comment..." 
+                    className="text-input"
+                    onChange={(e) => handleTextChange(e.target.value) }/> 
             <h4>Username</h4>
-            <input placeholder="name" onChange={(e) => handleNameChange(e.target.value) }/>
-            <h2><button onClick={handleClick}>Submit</button></h2>
+            <input className="name-input" 
+                    placeholder="name" 
+                    onChange={(e) => handleNameChange(e.target.value) }/>
+            <h2><button onClick={handleClick}>Post</button></h2>
         </div>        
     )
 }
